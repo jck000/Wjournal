0583adced8c4eb524b99a7326cfaa8ee382688dc
John Barrett <johna.barrett@gmail.com>
2013-04-03 19:55:58 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# NAME
+
+Wjournal - Blah blah blah
+
+# SYNOPSIS
+
+    use Wjournal;
+
+# DESCRIPTION
+
+Wjournal is
+
+# AUTHOR
+
+John Barrett <johna.barrett@gmail.com>
+
+# COPYRIGHT
+
+Copyright 2013- John Barrett
+
+# LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# SEE ALSO
---
+++ lib/Wjournal.pm
@@ -0,0 +1,39 @@
+package Wjournal;
+
+use strict;
+use 5.008_005;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Wjournal - Blah blah blah
+
+=head1 SYNOPSIS
+
+  use Wjournal;
+
+=head1 DESCRIPTION
+
+Wjournal is
+
+=head1 AUTHOR
+
+John Barrett E<lt>johna.barrett@gmail.comE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2013- John Barrett
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+=cut
