cd29dd55e5e1abd7a160e441c44a721d09a31a4b
John Barrett <johna.barrett@gmail.com>
2013-07-09 22:33:47 +0100

    Launcher script - has some CSRF skeleton in it, probably to be discarded

---
+++ bin/app.pl
@@ -0,0 +1,33 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+#use Plack::Builder;
+#use Dancer2::Request;
+use Dancer2 ':syntax';
+use Wjournal;
+
+#my $app = sub {
+#    use DDP; p @_;
+#    Dancer2->dance(Dancer2::Request->new(@_));
+#};
+#
+#
+#builder {
+#  enable 'Session';
+#  enable 'CSRFBlock',
+#    parameter_name => 'csrf_secret',
+#    token_length => 20,
+#    session_key => 'csrf_token',
+#    blocked => sub {
+#      [302, [Location => 'http://www.google.com'], ['']];
+#    },
+#    onetime => 0,
+#    ;
+#    $app;
+#    Dancer2->dance(Dancer2::Request->new(@_));
+#};
+
+dance;
+
